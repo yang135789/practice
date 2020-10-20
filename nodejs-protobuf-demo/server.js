43f667933200a44f6e3d85f546487040f39a2694
yangyongfeng <yangyongfeng@miya818.com>
2020-10-20 20:06:54 +0800

    nodejs-protobuf修改

--- nodejs-protobuf-demo/server.js
+++ nodejs-protobuf-demo/server.js
@@ -1,5 +1,7 @@
 let express = require('express');
 let protobuf = require('protobufjs');
+let fs = require('fs');
+let path = require('path');
 let app = express();
 let Root = protobuf.Root;
 let root = Root.fromJSON(require('./proto/index.json'));
@@ -25,6 +27,9 @@ function deCodeData(desction, data) {
 }
 
 app.use(express.raw({ type: 'application/protobuf' })) // 自定義解析application/protobuf為二進制數據
+app.use('/node_modules', express.static('./node_modules')) // 開放訪問權限
+app.use('/proto', express.static('./proto')) // 開放訪問權限
+app.use('/', express.static('./view')) // 開放訪問權限
 
 app.all('*', function (req, res, next) {
   //设置允许跨域的域名，*代表允许任意域名跨域
@@ -33,14 +38,15 @@ app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
   //跨域允许的请求方式 
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  // res.header("Content-Type", 'application/protobuf');
   next();
 });
 
-app.post('/', (req, res) => {
-  req.accepts('application/protobuf')
+app.post('/api', (req, res) => {
+  // req.accepts('application/protobuf')
   console.log(deCodeData('LoginReq', req.body));
   console.log(enCodeData('LoginRes', {province: 'va', city: 'cdacs', country: 'ccdace'}))
+  res.type('application/octet-stream');
+  // res.header("Content-Type", 'application/octet-stream');
   res.end(enCodeData('LoginRes', {province: 'va', city: 'cdacs', country: 'ccdace'}));
 })
 
