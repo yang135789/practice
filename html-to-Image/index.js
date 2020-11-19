d56104329e3b167337674caabff1aefe3e1b617e
yangyongfeng <yangyongfeng@miya818.com>
2020-11-19 20:20:47 +0800

    瀑布流修改

--- html-to-Image/index.js
+++ html-to-Image/index.js
@@ -10,26 +10,58 @@ function image2Base64(img) {
   return dataURL;
 }
 
+// 獲取元素樣式
+function getDomStyle (el) {
+  let styleSheetList = window.getComputedStyle(el, null);
+  let hasList = ['width','borderWidth','height','padding','paddingTop','paddingLeft','paddingRight','paddingBottom','margin','marginTop','marginLeft','marginRight','marginBottom'];
+  let data = {};
+  hasList.forEach(key => {
+    data[key] = parseInt(styleSheetList[key]);
+  })
+  data.totalHeight = data.paddingTop + data.paddingBottom + data.marginTop + data.marginBottom + data.height + data.borderWidth * 2;
+  data.totalWidth = data.paddingLeft + data.paddingRight + data.marginLeft + data.marginRight + data.width + data.borderWidth * 2;
+  return data
+}
+
 function createImageList(option) {
-  let el = option.el;
-  let images = option.images;
-  let imgWidth = option.imgWidth || 200;
-  var elWidth = el.offsetWidth;
-  var colspanCount = parseInt(elWidth / imgWidth);
-  console.log(el, elWidth, colspanCount)
+  let el = option.el; // 父元素
+  let rootStyle = {}; // 容器寬度
+  let images = option.images; // 圖片列表
+  let imgWidth = option.imgWidth || 200; // 圖片寬度
+  let colspanCount =0; // 計算列數
+  let imgHeightList = []; // 保存列高度
+  let imgLoadList = []; // 保存圖片加載列表
+  let loadImgIndex = 0;
   images.forEach(url => {
-    var imageContainer = document.createElement("div");
-    imageContainer.style.width = `max-content`;
-    imageContainer.style.height = `max-content`;
+    let imgBox = document.createElement("div");
     let img = new Image();
     img.src = url;
-    img.width = imgWidth;
+    imgBox.style.position = 'absolute';
+    imgBox.style.display = 'none';
+    imgBox.appendChild(img);
+    el.appendChild(imgBox);
     img.onload = function () {
-      // console.log(img.width, img.height);
-      // imageContainer.style.height = `${Math.ceil(imgWidth / img.width * img.height)}px`
+      imgBox.style.height = Math.ceil(img.height / img.width * imgWidth) + "px";
+      imgBox.style.width = imgWidth + "px";
+      console.log(img.height, img.width);
+      let imgBoxStyle = getDomStyle(imgBox); // 獲取圖片容器樣式
+      if (!rootStyle.width) {
+        rootStyle = getDomStyle(el);
+        colspanCount = parseInt(rootStyle.width / imgBoxStyle.totalWidth);
+      }
+      if(loadImgIndex < colspanCount){ // 第一行
+        imgHeightList.push(imgBoxStyle.totalHeight);
+        imgBox.style.left = imgBoxStyle.totalWidth * loadImgIndex + "px";
+      } else {
+        var minIndex = imgHeightList.indexOf(Math.min.apply(null,imgHeightList)); // 獲取最小高度一列索引值
+        imgBox.style.left = imgBoxStyle.totalWidth * minIndex + "px";
+        imgBox.style.top = imgHeightList[minIndex] + "px";
+        imgHeightList[minIndex] += imgBoxStyle.totalHeight;
+      }
+      imgLoadList.push({el: imgBox, style: imgBoxStyle});
+      imgBox.style.display = 'block';
+      el.style.height = Math.max.apply(null,imgHeightList) + 'px';
+      loadImgIndex++;
     };
-    imageContainer.appendChild(img);
-    el.appendChild(imageContainer);
   })
-  console.log(option);
 }
