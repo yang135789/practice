1724fa62db87f228e8b2a41ab3bf451818fb1c3f
Yang <yang6450867@gmail.com>
2020-12-09 02:30:04 +0800

    proto提取

--- pb-simplify/simplify.js
+++ pb-simplify/simplify.js
@@ -14,13 +14,16 @@ files.forEach(fileName => {
   fileCache[prefix] = splitProto(fileName, fileData)
 })
 
-// Object.keys(useFun).forEach(key => {
-//   useFun[key].forEach(fun => {
-//     let funReg = new RegExp(`rpc\\s${fun}\\s*\\((\\w+)\\)\\s*returns\\s*\\((\\w+)\\)`, 'g'); // 匹配方法
-//     console.log(fun);
-//     console.log(funReg.test(fileCache[key]));
-//   })
-// });
+Object.keys(useFun).forEach(key => {
+  useFun[key].forEach(funName => {
+    let funReg = new RegExp(`rpc\\s${funName}\\s*\\((\\w+)\\)\\s*returns\\s*\\((\\w+)\\)`, 'g'); // 匹配方法
+      let req = fileCache[key].rpcs[funName].req;
+      let res = fileCache[key].rpcs[funName].res;
+      let reqMsg = fileCache[key].messages[req];
+      let resMsg = fileCache[key].messages[res];
+      console.log(funName, req, res, reqMsg, resMsg, fileCache[key].messages['AssetsMoney']);
+  })
+});
 
 // let protoFiles = files.filter(str => /\.proto$/.test(str)).map(name => path.resolve(src, name));
 // pbjs.main(['--target', 'json', '--out', src + '/index.json'].concat(protoFiles), function (err, output) {
@@ -34,27 +37,49 @@ function splitProto (fileName, fileData) {
   let type = ['double', 'float', 'int32', 'int64', 'uint32', 'uint64', 'sint32', 'sint64', 'fixed32', 'fixed64', 'sfixed32', 'sfixed64', 'bool', 'string', 'bytes'];
   // 匹配rpc
   // let rpcReg = (name) => new RegExp(`rpc\\s${name}\\s*\\((\\w+)\\)\\s*returns\\s*\\((\\w+)\\)`, 'g'); 
+  let rpcs = {};
   let rpcReg = /rpc\s+(\w+)\s*\((\w+)\)\s*returns\s*\((\w+)\)\s*;/g; 
   // 匹配enum内容
+  let enums = {};
   let enumReg = /(?<!\bmessage\s+\w+\s*\{[^\{\}]*)\benum\s*\w+\s*\{[^\}]*\}/g;
+  let enumNameReg = /\benum\s*(\w+)\s*\{[^\}]*\}/g
   // 匹配message内容
+  let messages = {};
   let messageReg = /\bmessage\s+\w+\s*\{(([^\{\}]*\benum\s*\w+\s*\{[^\}]*\}[^\}]*\})|([^\}]*\}))/g;
+  let messageNameReg = /\bmessage\s+(\w+)\s*\{/g
   // 匹配service内容
   let serviceReg = /\bservice\s+\w+\s*\{[^\}]*\}/g; // 匹配proto文件service内容
   // 匹配头部引入声明内容
+  let header = {};
   let keyWord = /(\bsyntax.*)|(\boption.*)|(\bpackage.*)|(\bimport.*)/g;
   
   // 去注释
   fileData = fileData.replace(/\/\/.*/gm, '');
   
-  let messages = fileData.match(messageReg);
-  let header = fileData.match(keyWord);
-  let enums = fileData.match(enumReg);
-  let rpcs = fileData.match(rpcReg);
-  // console.log(fileName, messages.join('\n'));
-  // messages && fs.writeFileSync(`${outPath}/messages.${fileName}`, messages.join('\n'));
-  // header && fs.writeFileSync(`${outPath}/header.${fileName}`, header.join('\n'));
-  // enums && fs.writeFileSync(`${outPath}/enums.${fileName}`, enums.join('\n'));
-  rpcs && fs.writeFileSync(`${outPath}/rpc.${fileName}`, rpcs.join('\n'));
-  return fileData.match(messageReg)
+  (fileData.match(keyWord) || []);
+  (fileData.match(messageReg) || []).forEach(str => {
+    messageNameReg.lastIndex = 0;
+    let [src, fun] = messageNameReg.exec(str);
+    messages[fun] = {src: str};
+  });;
+  (fileData.match(enumReg) || []).forEach(str => {
+    enumNameReg.lastIndex = 0;
+    let [src, name] = enumNameReg.exec(str);
+    enums[name] = src;
+  });
+  (fileData.match(rpcReg) || []).forEach(str => {
+    rpcReg.lastIndex = 0;
+    let [src, fun, req, res] = rpcReg.exec(str);
+    rpcs[fun] = {src, req, res};
+  });
+  let services = {};
+  fileData.match(serviceReg);
+  return {
+    fileName,
+    services: services ? services[0] : '',
+    messages,
+    header,
+    enums,
+    rpcs
+  }
 }
