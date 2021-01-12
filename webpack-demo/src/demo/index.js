9f9bdb5c3e8c8716e6547a85bf5afff4b8c97a5d
yangyongfeng <yangyongfeng@miya818.com>
2021-01-12 20:39:41 +0800

    mp3可视化例子

--- webpack-demo/src/demo/index.js
+++ webpack-demo/src/demo/index.js
@@ -1,25 +1,5 @@
 import { createApp } from 'vue';
 import index from './index.vue';
+import '@src/assets/style/index.scss'
 
 let app = createApp(index).mount('#app');
-console.log(app);
-// function a () {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       resolve(this);
-//     })
-//   })
-// }
-// async function b () {
-//   let obj = await a();
-//   console.log(obj);
-// }
-
-// b();
-
-// let c = {};
-
-// c?.b?.a ?? console.log(a);
-
-let obj = {a: 1}; 
-console.log(Object.values(obj));
--- webpack-demo/src/demo/index.vue
+++ webpack-demo/src/demo/index.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="home">
-    <mp3Play></mp3Play>
-    <!-- <mp3Play1></mp3Play1> -->
     <div class="pathTest">
       <img src="./assets/image/29.jpg">
-      <img src="/static/29.jpg">
-      <img src="./assets/image/pause.png">
     </div>
+    <mp3PlayToAudioTag></mp3PlayToAudioTag>
+    <div class="pathTest">
+      <img src="/static/29.jpg">
+    </div>
+    <mp3PlayToAudioJs></mp3PlayToAudioJs>
   </div>
 </template>
 <script>
-import mp3Play from './components/mp3Play'
-// import mp3Play1 from './components/mp3Play1'
+import mp3PlayToAudioTag from './components/mp3PlayToAudioTag'
+import mp3PlayToAudioJs from './components/mp3PlayToAudioJs'
 export default {
-  components: {mp3Play},
+  components: {mp3PlayToAudioTag, mp3PlayToAudioJs},
   data () {
     return {
       tag: '标签'
@@ -34,19 +35,12 @@ export default {
 
 <style lang="scss" scoped>
 .home {
-  width: 500px;
-  height: 300px;
   .pathTest {
     width: 300px;
+    margin-bottom: 20px;
     img {
       max-width: 100%;
     }
-  }
-  h1 {
-    color: #bcad;
-  }
-  p {
-    color: #deac;
   }
 }
 </style>
