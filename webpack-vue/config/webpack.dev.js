57b3576b99b09301ce2530551bd07b04cc6ca157
yangyongfeng <yangyongfeng@miya818.com>
2021-02-05 16:19:03 +0800

    webpack-merge變更

--- webpack-vue/config/webpack.dev.js
+++ webpack-vue/config/webpack.dev.js
@@ -1,10 +1,11 @@
-const merge = require('webpack-merge');
+const webpackMerge = require('webpack-merge');
 
+console.log(merge)
 module.exports = env => {
   // 获取环境变量，匹配: 开头的键作为项目文件名
   global.proj = Object.keys(env).filter(key => /^:.*/.test(key))[0].slice(1);
   const common = require('./webpack.common'); // 公用配置， 因为用到global.proj变量，所以在后面赋值后引入
-  return merge(common, {
+  return webpackMerge.merge(common, {
     // 原始源代码（仅限行）
     devtool: 'cheap-module-source-map',
     mode:'development', // 模式,'development' 开发 或 'production'生产
--- webpack-vue/config/webpack.prod.js
+++ webpack-vue/config/webpack.prod.js
@@ -1,9 +1,9 @@
-const merge = require('webpack-merge'); // 配置合并方法
+const webpackMerge = require('webpack-merge'); // 配置合并方法
 
 module.exports = env => {
   global.proj = Object.keys(env).filter(key => /^:.*/.test(key))[0].slice(1);
   const common = require('./webpack.common.js'); // 公用配置， 因为用到global.proj变量，所以在后面赋值后引入
-  return merge(common, {
+  return webpackMerge.merge(common, {
     mode: 'production', // 生產模式
     devtool: false, // map映射
   });
