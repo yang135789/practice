c8aa4aea65ec500207a1ca0908a2b98217697ae7
yangyongfeng <yangyongfeng@miya818.com>
2021-02-07 17:32:23 +0800

    修改

--- webpack-vue/src/audioPlayer/components/mp3PlayToAudioJsRe.vue
+++ webpack-vue/src/audioPlayer/components/mp3PlayToAudioJsRe.vue
@@ -7,11 +7,16 @@
     <div class="ctrl">
       <div  :class="[isPlay ? 'pause' : 'play']" @click="clickStart('start')"></div>
       <div class="time">{{ currentTime }}</div>
-      <div class="timeBar" @click="changeTime">
-        <div class="timeBarLength" :style="{width: `${time / duration * 100}%`}"></div>
+      <div class="timeline" @click="changeTime">
+        <div class="timelineLength" :style="{width: `${time / duration * 100}%`}"></div>
       </div>
       <div class="voices" @click="changeVoices">
         <div class="voicesBar" :style="{left: `${voicesSize * 100}%`}"></div>
+      </div>
+    </div>
+    <div class="list">
+      <div v-for="(item, key) in list" :key="key" :class="{active: key === selectAudio}" @click="clickAudioList(key)">
+        {{ key }}
       </div>
     </div>
   </div>
@@ -22,7 +27,6 @@ export default {
   data () {
     return {
       isPlay: false, // 正在播放
-      audio: null,
       audioCtx: null,
       duration: 0,
       time:  0, // 播放時長
@@ -30,22 +34,26 @@ export default {
       analyser: null,
       config: { attributes: true, childList: true, subtree: true },
       arr: new Array(16).fill(),
-      // browser: Browser.getBrowser(), // 判斷瀏覽器
-      index: 0,
+      index: 0, // 動畫定時器
       map3Buffer: null, // mp3數據
-      showTip: true,
+      audioCache: {}, // 緩存mp3數據
       isLoading: false, // 加載動畫
       gainNode: null, // 音量控制
-      voicesSize: 1,
+      voicesSize: 1, // 聲音長度
+      selectAudio: '光良 - 童话',
+      num: 3 // 柱子數量
     }
   },
-  mounted () {
+  created () {
+    this.initAudio();
   },
   computed: {
-    src () {
-      return `${location.origin}${location.pathname.replace('/index.html', '/')}static/光良 - 童话.mp3?t=${window.timestamp}`
-      // return `${location.origin}${location.pathname.replace('/index.html', '/')}static/麦振鸿 - 莫失莫忘(逍遥叹 演奏曲).mp3?t=${window.timestamp}`
-      // return `${location.origin}${location.pathname.replace('/index.html', '/')}static/Lupins.mp3?t=${window.timestamp}`
+    list () {
+      return {
+        'Lupins' : `${location.origin}${location.pathname.replace('/index.html', '/')}static/Lupins.mp3?t=${window.timestamp}`,
+        '光良 - 童话': `${location.origin}${location.pathname.replace('/index.html', '/')}static/光良 - 童话.mp3?t=${window.timestamp}`,
+        '麦振鸿 - 莫失莫忘(逍遥叹 演奏曲)': `${location.origin}${location.pathname.replace('/index.html', '/')}static/麦振鸿 - 莫失莫忘(逍遥叹 演奏曲).mp3?t=${window.timestamp}`,
+      }
     },
     // 当前播放时间
     currentTime () {
@@ -67,7 +75,7 @@ export default {
           resolve(xhr.response);
         };
         xhr.onerror = function (err) {
-          reject({status: xhr.status, des: xhr.response})
+          reject({status: xhr.status, des: xhr.response});
         }
         xhr.open(type, url);
         options.headers && Object.keys(options.headers).forEach(key => {
@@ -78,67 +86,36 @@ export default {
       })
     },
     // 获取音乐数据
-    getAudioData () {
+    getAudioData (url) {
       this.isLoading = true;
-      this.ajax({
-        url: this.src,
+      return this.ajax({
+        url: url,
         responseType:'arraybuffer'
-      }).then((data) => {
-        this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-        this.analyser = this.audioCtx.createAnalyser();
-        this.analyser.fftSize = 512 / 4;
-        this.audioCtx.decodeAudioData(data, (buffer) => {
-          this.map3Buffer = buffer;
-          this.duration = Math.round(buffer.duration);
-          this.time = this.duration;
-          this.isLoading = false;
-          this.clickStart();
-          console.log('加載結束');
-        }, function(e){"Error with decoding audio data" + e.err});
+      }).then(async (data) => {
+        return await this.decodeAudioData(data);
       }).catch(err => {
         console.log('獲取音頻失敗!', err);
+      }).finally(() => {
+        this.isLoading = false;
       })
     },
-    clickStart () {
-      if (!this.map3Buffer) { // 如果沒有音樂
-        this.isLoading || this.getAudioData(); // 獲取音樂數據
-        return
+    async clickStart () {
+      if (!this.audioCache[this.selectAudio] && !this.isLoading) { // 如果沒有音樂
+        await this.getAudioData(this.list[this.selectAudio]); // 獲取音樂數據
+        // this.clickStart();
+        // return
       }
       if (this.isPlay) { // 正在播放
         this.isPlay = false; // 修改播放狀態
         this.audioCtx.suspend(); // 暫停
         cancelAnimationFrame(this.index); // 清楚動畫定時器
       } else {
-        this.showTip = false;
-        if (!this.AudioBufferSourceNode) { // 
+        if (!this.AudioBufferSourceNode) {
           this.startPlay()
-          // this.AudioBufferSourceNode = this.audioCtx.createBufferSource();
-          // // this.AudioBufferSourceNode.playbackRate // 速度控制
-          // this.AudioBufferSourceNode.onended = () => { // 音樂播放結束觸發
-          //   console.log('結束播放');
-          //   cancelAnimationFrame(this.index); // 清除動畫定時器
-          //   this.arr = new Array(16).fill();
-          //   this.audioCtx.suspend(); // 暫停
-          //   // this.audioCtx.listener.positionX.value = 0
-          //   this.time = this.duration;
-          //   this.AudioBufferSourceNode = null;
-          //   this.isPlay = false;
-          // }
-          // this.gainNode = this.audioCtx.createGain();
-          // this.AudioBufferSourceNode.buffer = this.map3Buffer;
-          // this.gainNode.connect(this.audioCtx.destination);
-          // this.AudioBufferSourceNode.connect(this.gainNode);
-          // this.AudioBufferSourceNode.connect(this.analyser);
-          // this.AudioBufferSourceNode.start(0);
-          // console.log(this.audioCtx, this.AudioBufferSourceNode.playbackRate);
         }
         this.audioCtx.resume();
         requestAnimationFrame(this.visualization);
       }
-    },
-    // 播放時間改變時觸發
-    timeupdate ({target}) {
-      this.time = target.currentTime;
     },
     // 获取可视化数据并渲染
     visualization () {
@@ -154,16 +131,17 @@ export default {
       } else {
         this.isPlay = false;
       }
-	    this.index = requestAnimationFrame(this.visualization);
+      this.index = requestAnimationFrame(this.visualization);
     },
-    changeTime (e) {
+    changeTime (e) { // 點擊事件軸
+      if (!this.AudioBufferSourceNode) return;
+      this.AudioBufferSourceNode.onended = () => {};
       this.AudioBufferSourceNode.stop();
-      this.startPlay (20);
-
-      // this.AudioBufferSourceNode.start(this.audioCtx.currentTime + 1,3,10);
-      // this.AudioBufferSourceNode.start(120);
+      let timeline = (e.offsetX / e.target.offsetWidth) * this.duration
+      this.startPlay(timeline);
+      console.log('修改時間軸', timeline);
     },
-    startPlay (time = 0) {
+    startPlay (timeline) {
       this.AudioBufferSourceNode = this.audioCtx.createBufferSource();
       // this.AudioBufferSourceNode.playbackRate // 速度控制
       this.AudioBufferSourceNode.onended = () => { // 音樂播放結束觸發
@@ -173,19 +151,46 @@ export default {
         this.audioCtx.suspend(); // 暫停
         // this.audioCtx.listener.positionX.value = 0
         this.time = this.duration;
-        this.AudioBufferSourceNode = null;
         this.isPlay = false;
+        this.AudioBufferSourceNode = null;
       }
-      this.gainNode = this.audioCtx.createGain();
-      this.AudioBufferSourceNode.buffer = this.map3Buffer;
-      this.gainNode.connect(this.audioCtx.destination);
+      this.AudioBufferSourceNode.buffer = this.audioCache[this.selectAudio];
       this.AudioBufferSourceNode.connect(this.gainNode);
       this.AudioBufferSourceNode.connect(this.analyser);
-      this.AudioBufferSourceNode.start(time);
+      this.AudioBufferSourceNode.start(0, timeline); // 延遲多少秒播放, 從多秒開始播, 播幾秒
     },
     changeVoices (e) { // 改變音量大小
        console.log(e)
        this.voicesSize = this.gainNode.gain.value = e.offsetX / e.target.offsetWidth;
+    },
+    // 初始化
+    initAudio () {
+      this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+      this.analyser = this.audioCtx.createAnalyser();
+      this.analyser.fftSize = 16*(2**this.num); // 可視化柱子數量
+      
+      this.gainNode = this.audioCtx.createGain();
+      this.gainNode.connect(this.audioCtx.destination);
+      console.log('柱子數量:', 8*(2**this.num))
+    },
+    // 點擊列表 
+    clickAudioList (name) {
+      this.selectAudio = name
+    },
+    //
+    decodeAudioData (data) {
+      return new Promise((resolve, reject) => {
+        this.audioCtx.decodeAudioData(data, (buffer) => {
+          this.map3Buffer = buffer;
+          this.audioCache[this.selectAudio] = buffer;
+          this.duration = Math.round(buffer.duration);
+          this.time = this.duration;
+          resolve();
+          console.log('加載結束');
+        }, function(e){
+          reject("Error with decoding audio data" + e.err)
+        });
+      })
     }
   },
 }
@@ -230,11 +235,11 @@ export default {
       color: #fff;
       font-size: 24px;
     }
-    .timeBar {
+    .timeline {
       width: 50%;
       height: 10px;
       background: cornflowerblue;
-      .timeBarLength {
+      .timelineLength {
         height: 100%;
         width: 0;
         background: crimson;
@@ -256,6 +261,17 @@ export default {
       }
     }
   }
-  
+  .list {
+    > div {
+      // height: 50px;
+      padding: 10px 20px;
+      box-sizing: border-box;
+      font-size: 28px;
+      &.active {
+        color: #fff;
+        background: darkmagenta;
+      }
+    }
+  }
 }
 </style>
