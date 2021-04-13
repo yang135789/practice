df3689f7d3d29c7932b41d736151fd294d3c14c3
Yang <yang6450867@gmail.com>
2021-04-13 18:31:04 +0800

    模板创建修改

--- webpack-vue/config/cli/generator.js
+++ webpack-vue/config/cli/generator.js
@@ -1,3 +1,26 @@
-const Yeoman = require('yeoman-environment');
+const Yeoman = require('yeoman-environment'); // 环境配置
 const Generator = require('yeoman-generator');
-Yeoman.createEnv()
+const path = require('path');
+module.exports = class extends Generator {
+  constructor (params) {
+    super([], { // 自定义环境
+      env: Yeoman.createEnv([], {
+        cwd: path.resolve('./src', params.name) // 目标目录
+      }),
+      resolved: path.resolve(__dirname, './templates') // 模板目录
+    });
+    this.params = params;
+    // console.log();
+  }
+  writing() {
+    this.fs.copyTpl(
+      this.templatePath(), // 模板
+      this.destinationPath(), // 目标
+      {
+        htmlTitle: '<%= htmlWebpackPlugin.options.title %>',
+        htmlTimestamp: '<%= htmlWebpackPlugin.options.timestamp %>',
+        htmlClearCache: '<%= htmlWebpackPlugin.options.clearCache %>'
+      }
+    );
+  }
+}
--- webpack-vue/config/cli/index.js
+++ webpack-vue/config/cli/index.js
@@ -2,18 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const { program } = require('commander'); // 命令行解析工具
 const inquirer = require('inquirer'); // 控制台交互工具
-const generator = require('./generator');
-
+const Generator = require('./generator'); 
 // program
 //   .version('0.0.1')
 //   .option('-pyf, --pyf <string>', 'Add project:year:folder')
 //   .parse(process.argv);
 // const options = program.opts();
 // console.log(options.pyf);
-
-
-function mkDir (src, des) {
-}
 
 // src目錄下的項目
 const srcList = fs.readdirSync(path.resolve(__dirname, '../../src'));
@@ -47,6 +42,7 @@ inquirer.prompt(prompt).then(res => {
   if (!fs.existsSync(proj)) {
     fs.mkdirSync(proj);
   }
-  console.log(res, proj)
+  const generator = new Generator(res);
+  generator.run();
 });
 // 解析命令行参数
