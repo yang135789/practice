74a8a91dccf33b7ad326d0009b371dbf8502ffc7
Yang <yang6450867@gmail.com>
2021-05-20 20:42:32 +0800

    鱼游动, 子弹方向

--- webpack-vue/src/canvasDemo/components/catchFishCvs.vue
+++ webpack-vue/src/canvasDemo/components/catchFishCvs.vue
@@ -12,6 +12,10 @@
 export default {
   data () {
     return {
+      imgs: {
+        fish: ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png'],
+        bullet: ['bullet.png']
+      },
       canvas: {
         el: null,
         ctx: null,
@@ -32,7 +36,7 @@ export default {
       },
       bulletData: {
         width: this.toCurPX(20),
-        height: this.toCurPX(20),
+        height: this.toCurPX(60),
         color: '#000',
         list: []
       },
@@ -44,8 +48,8 @@ export default {
       },
       gunData: { // 炮弹数据
         color: "#fac",
-        height: this.toCurPX(50),
-        width: this.toCurPX(50),
+        height: this.toCurPX(60),
+        width: this.toCurPX(20),
         centerX: 0, // 原坐标x
         centerY: 0, // 原坐标y
         targFishList: [], // 目标线上的鱼
@@ -54,13 +58,45 @@ export default {
     }
   },
   computed: {
-  },
-  mounted () {
+    },
+  async mounted () {
+    await this.loadImgs();
     this.$nextTick(() => {
       this.init();
     })
   },
   methods: {
+    // 加载图片
+    loadImgs () {
+      return new Promise((res, rej) => {
+        let count = 0;
+        let total = 0;
+        let errorImg = [];
+        for (let key in this.imgs) {
+          total += this.imgs[key].length;
+          this.imgs[key].forEach((imgStr, index) => {
+            let img = new Image();
+            img.src = require(`../assets/image/${imgStr}`);
+            img.onload = () => {
+              count++;
+              this.imgs[key][index] = img;
+              if (count >= total) {
+                res(errorImg);
+                console.log('加载完成');
+              }
+            }
+            img.onerror = () => {
+              total --;
+              errorImg.push(imgStr)
+              if (count >= total) {
+                res(errorImg);
+                console.log('加载完成');
+              }
+            }
+          })
+        }
+      })
+    },
     toCurPX (px) { // 轉換為當前屏幕大小的像素
       return px / (1920 / 100) * (window.innerWidth / 100);
     },
@@ -71,6 +107,7 @@ export default {
         this.canvas.el.width = this.canvas.width;
         this.canvas.el.height = this.canvas.height;
       }
+      this.bulletData.list = [];
       this.initFish();
       this.initGun();
       this.draw();
@@ -79,11 +116,7 @@ export default {
     initFish () {
       this.fishData.list = [];
       // 初始化矩阵数据
-      let imgs = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png']
       this.fishData.matrix.forEach((fishRow, index) => {
-        let img = new Image();
-        img.src =  require(`../assets/image/${imgs[index]}`);
-        // document.body.append(img);
         fishRow.forEach((fish, i) => {
           let r = 255 * Math.random();
           let g = 255 * Math.random();
@@ -106,10 +139,50 @@ export default {
           fish.centerY = fish.y + fish.height / 2;
           // 死亡
           fish.isDead = false;
-          // 图片
-          fish.img = img;
-          //
-          fish.item = []
+          // 内容
+          let itemImg = this.imgs.fish[index];
+          // let itemHeight = 0.5 * fish.height;
+          // let itemWidth = itemHeight / itemImg.height * itemImg.width;
+          let itemWidth = 0.5 * fish.width;
+          let itemHeight = itemWidth / itemImg.width * itemImg.height;
+          let itemX = fish.centerX - itemWidth / 2;
+          let itemY = fish.centerY - itemHeight / 2;
+          let itemX1 = itemX + itemWidth;
+          let itemY1 = itemY + itemHeight;
+          fish.item = {
+            img: itemImg,
+            height: itemHeight,
+            width: itemWidth,
+            x: itemX,
+            y: itemY,
+            x1: itemX1,
+            y1: itemY1,
+            stepX: Math.random(),
+            stepY: Math.random(),
+            isPlay: false,
+            play: () => {
+              let item = fish.item;
+              if (item.isPlay) return;
+              // if (!(fish.row === 0 && fish.cel === 1)) return;
+              item.isPlay = true;
+              // item.x+=50;
+              let timer = this.fps60Timer(() => {
+                if (item.y1 + item.stepY > fish.y1 || item.y + item.stepY < fish.y) {
+                  item.stepY = -item.stepY;
+                }
+                if (item.x1 + item.stepX > fish.x1 || item.x + item.stepX < fish.x) {
+                  item.stepX = -item.stepX;
+                }
+                item.x+=item.stepX;
+                item.x1+=item.stepX;
+                item.y+=item.stepY;
+                item.y1+=item.stepY;
+                if (fish.isDead || !this.canvas.runing) {
+                  clearInterval(timer)
+                }
+              }, 60)
+            },
+          }
           this.fishData.list.push(fish);
         })
       });
@@ -118,33 +191,42 @@ export default {
     initGun () {
       this.gunData.x = this.canvas.width / 2 - this.gunData.width / 2;
       this.gunData.y = this.canvas.height - this.gunData.height;
+      this.gunData.img = this.imgs['bullet'][0];
       this.gunData.centerX = this.canvas.width / 2;
       this.gunData.centerY = this.canvas.height - this.gunData.height / 2;
     },
     // 60帧定时器
-    fps60Timer (fn) {
-      return setInterval(fn, 1000 / 60);
+    fps60Timer (fn, fps = 60) {
+      return setInterval(fn, 1000 / fps);
     },
     // 渲染画布
     draw (times) {
-      // this.canvas.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
-      this.canvas.ctx.fillStyle = 'rgba(255,255,255,0.3)';
-      this.canvas.ctx.fillRect(0,0,this.canvas.width,this.canvas.height);
-      // 绘制鱼
-      this.fishData.list.forEach((fish, index) => {
-        if (!fish.isDead) {
-          this.canvas.ctx.drawImage(fish.img, fish.x, fish.y)
-          this.canvas.ctx.fillStyle = fish.color;
-          this.canvas.ctx.fillRect(fish.x, fish.y, fish.width, fish.height);
-        }
-      });
+      this.canvas.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
+      // this.canvas.ctx.fillStyle = 'rgba(255,255,255,0.3)';
+      // this.canvas.ctx.fillRect(0,0,this.canvas.width,this.canvas.height);
       // 绘制枪
       this.canvas.ctx.fillStyle = this.gunData.color;
       this.canvas.ctx.fillRect(this.gunData.x, this.gunData.y, this.gunData.width, this.gunData.height);
+      this.canvas.ctx.drawImage(this.gunData.img, this.gunData.x, this.gunData.y, this.gunData.width, this.gunData.height)
+      // 绘制鱼
+      this.fishData.list.forEach((fish, index) => {
+        if (!fish.isDead) {
+          this.canvas.ctx.fillStyle = fish.color;
+          this.canvas.ctx.fillRect(fish.x, fish.y, fish.width, fish.height);
+          // 渲染鱼图片
+          this.canvas.ctx.drawImage(fish.item.img, fish.item.x, fish.item.y, fish.item.width, fish.item.height)
+          times && fish.item.play()
+        }
+      });
       // 绘制子弹
       this.canvas.ctx.fillStyle = this.bulletData.color;
       this.bulletData.list.forEach(item => {
-        this.canvas.ctx.fillRect(item.x, item.y, this.bulletData.width, this.bulletData.height);
+        this.canvas.ctx.save();
+        this.canvas.ctx.translate(item.centerX, item.centerY);
+        this.canvas.ctx.rotate(item.deg);
+        this.canvas.ctx.drawImage(item.img, -this.bulletData.width / 2, -this.bulletData.height / 2, this.bulletData.width, this.bulletData.height);
+        this.canvas.ctx.restore();
+        // this.canvas.ctx.fillRect(item.x, item.y, this.bulletData.width, this.bulletData.height);
       })
       if (times) {
         // 记录时间戳
@@ -152,6 +234,9 @@ export default {
         this.timeData.runTime = times - this.timeData.timestamp;
         this.timeData.time = Math.ceil(this.timeData.gameLength - this.timeData.runTime / 1000);
         this.canvas.animationFrame = requestAnimationFrame(this.draw);
+        if (this.timeData.time <= 0) {
+          this.stop()
+        }
       }
     },
     // 获取坐标点上的鱼
@@ -162,7 +247,6 @@ export default {
     // 点击画布
     clickCanvas (event) {
       if (!this.canvas.runing) return;
-      let targetFish = this.getXYofFish(event.offsetX, event.offsetY);
       // 子弹终点
       let bulletEnd = this.distanceToXY(event.offsetX, event.offsetY, this.canvas.height + 100);
       let bullet = this.createBullet(bulletEnd.x, bulletEnd.y);
@@ -171,8 +255,12 @@ export default {
     // 生成子弹
     createBullet (targX, targY) {
       // 在弹道上的鱼
+      // let targetFish = this.getXYofFish(targX, targY);
       let routerFish = this.forecastRoute(targX, targY);
+      
       let bullet = {
+        centerX: this.gunData.centerX,
+        centerY: this.gunData.centerY,
         x: this.gunData.centerX - this.bulletData.width / 2,
         y: this.gunData.centerY - this.bulletData.height / 2,
         x1: this.gunData.centerX + this.bulletData.width / 2,
@@ -183,8 +271,11 @@ export default {
         curY: this.gunData.centerY - this.bulletData.height,
         timmer: null,
         invalid: false, // 子弹失效
-        fishList: routerFish // 弹道上的鱼列表
+        fishList: routerFish, // 弹道上的鱼列表
+        img: this.imgs['bullet'][0],
+        deg: Math.atan2((targY - this.gunData.centerY), (targX - this.gunData.centerX)) + Math.PI / 2
       }
+      console.log(bullet.deg);
       let s = 3; // 小球到终点所用时间
       let x = bullet.targX - bullet.x;
       let y = bullet.targY - bullet.y;
@@ -197,6 +288,8 @@ export default {
         bullet.y += stepY;
         bullet.x1 += stepX;
         bullet.y1 += stepY;
+        bullet.centerX += stepX;
+        bullet.centerY += stepY;
         let overstepX = stepX >= 0 ? bullet.x >= bullet.targX : bullet.x <= bullet.targX;
         let overstepY = bullet.y <= bullet.targY;
         // 遍历经过哪些鱼，跳过打过的鱼
